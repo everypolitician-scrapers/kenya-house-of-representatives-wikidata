a16b6f6c7ed1928b98ceffe88b73921caafb991f
Tony Bowden <tony@tmtm.com>
2015-10-03 11:52:42 +0100

    Fetch via Category lookup
    No need to parse the page — they're in a Category

--- scraper.rb
+++ scraper.rb
@@ -3,26 +3,12 @@
 
 require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
-end
+@pages = [
+  'Category:Members_of_the_11th_Parliament_of_Kenya'
+]
 
-def wikinames(url)
-  noko = noko_for(url)
-  noko.xpath('//table[.//th[contains(.,"Representative-elect")]]//tr[td]').map { |tr| tr.css('td')[-2].xpath('.//a[not(@class="new")]/@title').text }.uniq
-end
-
-names = wikinames('https://en.wikipedia.org/wiki/Kenya_National_Assembly_elections,_2013')
-
-WikiData.ids_from_pages('en', names).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('fr') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
+@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each do |id|
+  data = WikiData::Fetcher.new(id: id).data or next
   ScraperWiki.save_sqlite([:id], data)
 end
