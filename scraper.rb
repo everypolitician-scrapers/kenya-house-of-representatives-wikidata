465a054a27293bdcea8568d511caf1e0497a93af
Tony Bowden <tony@tmtm.com>
2016-01-12 17:05:26 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'rest-client'
 
-@pages = [
-  'Category:Members_of_the_11th_Parliament_of_Kenya'
-]
-
-@pages.map { |c| WikiData::Category.new(c, 'en').wikidata_ids }.flatten.uniq.each do |id|
-  data = WikiData::Fetcher.new(id: id).data or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = WikiData::Category.new('Category:Members_of_the_11th_Parliament_of_Kenya', 'en').member_titles 
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
