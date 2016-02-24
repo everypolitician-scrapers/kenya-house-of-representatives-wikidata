4fc06e9968d5502696d1f1b10a5eb9b06ab9a59a
Tony Bowden <tony@tmtm.com>
2016-02-24 08:50:12 +0000

    Fetch via template

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,13 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Category:Members_of_the_11th_Parliament_of_Kenya', 'en').member_titles 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+category_names = WikiData::Category.new('Category:Members_of_the_11th_Parliament_of_Kenya', 'en').member_titles 
+
+template_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/Template:Current_MPs_of_Kenya&limit=500',
+  xpath: '//ul[@id="mw-whatlinkshere-list"]//li//a[not(@class="new")][1]/@title',
+).uniq.reject { |n| n.include? ':' }
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: category_names | template_names })
 warn EveryPolitician::Wikidata.notify_rebuilder
 
